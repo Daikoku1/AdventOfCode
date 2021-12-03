f3bd4bd8ca6ab954845be9993030d9b4ca80394f
daikoku <jjys9047@naver.com>
2021-12-03 17:30:22 +0900

    start project day1 end day2 end day3 end

---
+++ 2021/day01.py
@@ -0,0 +1,11 @@
+import os
+with open('2021/input/day1.txt') as f:
+    lines = list(map(int, f.readlines()))
+
+# part1
+print(sum([i < j for i, j in zip(lines[:-1], lines[1:])]))
+
+
+# part2
+three_sums = [sum(lines[i:i+3]) for i in range(len(lines) - 2)]
+print(sum([i < j for i, j in zip(three_sums[:-1], three_sums[1:])]))
---
+++ 2021/day02.py
@@ -0,0 +1,26 @@
+import os
+with open('2021/input/day2.txt') as f:
+    lines = list(map(lambda x : x.strip(), f.readlines()))
+
+# Part1
+x, y = 0, 0
+for l in lines:
+    c, n = l.split()
+    n = int(n)
+    if c == 'up' : y -= n
+    elif c == 'down' : y += n
+    else: x += n
+print(x*y)
+
+
+# Part1
+x, y, aim = 0, 0, 0
+for l in lines:
+    c, n = l.split()
+    n = int(n)
+    if c == 'up' : aim -= n
+    elif c == 'down' : aim += n
+    else:
+        x += n
+        y += aim * n
+print(x*y)
